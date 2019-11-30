59349c8b0611f319fc942d408bac9fb21eb2388d
csamyphew <csamyphew.cky@gmail.com>
2019-12-01 04:26:55 +0800

    Fix checkbox model and require

--- src/components/FoodDetail.vue
+++ src/components/FoodDetail.vue
@@ -14,15 +14,15 @@
       <b-form-group id="input-group-1" label="Qty" label-for="input-1">
         <b-form-input id="input-1" v-model="form.quantity" required type="number" min="1" step="1" />
       </b-form-group>
-      <b-form-group :id="'input-group-'+index+1" :key="index" v-for="(item,index) in this.attrOptions" :label="item.title.text">
-        <b-form-checkbox-group  v-if="item.multiple" :required="item.mandatory"
+      <b-form-group :id="'input-group-'+index+1" :key="index" v-for="(item,index) in this.attrOptions" :label="item.label">
+        <b-form-checkbox-group  v-if="item.multiple"
           v-model="form.attrs[index]['options']" :options="item.inputOptions" :name="'input-group-'+index+1">
         </b-form-checkbox-group>
         <b-form-radio-group v-else :required="item.mandatory"
           v-model="form.attrs[index]['options'][0]" :options="item.inputOptions" :name="'input-group-'+index+1">
         </b-form-radio-group>
       </b-form-group>
-
+      <b-form-invalid-feedback :state="!form.error">Please select</b-form-invalid-feedback>
       <div class="col-12 mt-3">
         <div class="row justify-content-between">
           <b-button variant="outline-secondary" @click="hideModal">Cancel</b-button>
@@ -50,7 +50,7 @@ export default {
       return require('@/assets/'+this.food.image)
     },
     attrOptions: function(){
-      return this.food.attr.map((item)=>{
+      return this.food.attr.map((item,index)=>{
         var inputOptions = item.item.map((option)=>{
           return{
             value: option,
@@ -59,6 +59,7 @@ export default {
         });
         return {
           ...item,
+          label: item.mandatory?item.title.text+"*(Mandatory)":item.title.text,
           inputOptions: inputOptions
         }
       });
@@ -67,38 +68,47 @@ export default {
   methods:{
     ...mapActions(['addFood']),
     addToBasket(evt){
-      evt.preventDefault()
-      console.log(JSON.stringify(this.form));
-      var item = {
-        id: this.food.id,
-        name: this.food.name,
-        price: this.food.price,
-        quantity: this.form.quantity,
-        attrs: this.form.attrs
+      evt.preventDefault();
+      this.formValidation();
+      if(!this.form.error){
+         var item = {
+          id: this.food.id,
+          name: this.food.name,
+          price: this.food.price,
+          quantity: this.form.quantity,
+          attrs: this.form.attrs
+        }
+        this.addFood(item);
+        this.hideModal();
       }
-      this.addFood(item);
-      this.hideModal();
-      this.form = this.initialistForm();
     },
     hideModal() {
-      this.$refs['food'+this.food.id+'detail-modal'].hide()
+      this.$refs['food'+this.food.id+'detail-modal'].hide();
+      this.form = this.initialistForm();
     },
     initialistForm(){
-      var form = { quantity: 1, attrs: [] };
+      var form = { quantity: 1, attrs: [], error: false };
       this.food.attr.forEach((item,index)=>{
-        if(item.multiple){
-          form.attrs.push({
-            name: item.title,
-            options: []
-          });
-        }else{
-          form.attrs.push({
-            name: item.title,
-            options: ['']
-          });
+        var optionItem = {
+          name: item.title,
+          mandatory: item.mandatory,
+          options:[]
         }
+        if(!item.multiple){
+          optionItem.options.push('');
+        }
+        form.attrs.push(optionItem);
       })
       return form;
+    },
+    formValidation(){
+      //reset error checking
+      this.form.error = false;
+      this.form.attrs.forEach((attr)=>{
+        if(attr.mandatory && attr.options.length==0){
+          this.form.error = true;
+        }
+      })
     }
   }
 
