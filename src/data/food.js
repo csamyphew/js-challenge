db56a17c8e080b80c812fafd7cd0ab1fb1494aad
csamyphew <csamyphew.cky@gmail.com>
2019-11-30 22:29:56 +0800

    Add dummy data

---
+++ src/data/food.js
@@ -0,0 +1,242 @@
+export const food_items = [{
+    "id": 1,
+    "name": "Milkshake",
+    "image": "../assets/milkshake.png",
+    "price": 30,
+    "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    "attr": [{
+        "mandatory": true,
+        "multiple": false,
+        "title": {
+          "key": "flavour",
+          "value": "Flavour"
+        },
+        "item": [{
+            "key": "chocolate",
+            "value": "Chocolate"
+          },
+          {
+            "key": "vanilla",
+            "value": "Vanilla"
+          },
+          {
+            "key": "strawberry",
+            "value": "Strawberry"
+          }
+        ]
+      },
+      {
+        "mandatory": true,
+        "multiple": false,
+        "title": "size",
+        "item": [{
+            "key": "small",
+            "value": "Small"
+          },
+          {
+            "key": "medium",
+            "value": "Medium"
+          },
+          {
+            "key": "large",
+            "value": "Large"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "id": 2,
+    "name": "Cale",
+    "image": "../assets/cake.png",
+    "price": 15,
+    "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    "attr": [{
+      "mandatory": true,
+      "multiple": false,
+      "title": {
+        "key": "flavour",
+        "value": "Flavour"
+      },
+      "item": [{
+          "key": "black-forest",
+          "value": "Black Forest"
+        },
+        {
+          "key": "mixed-fruit",
+          "value": "Mixed fruit"
+        },
+        {
+          "key": "marble-cheese",
+          "value": "Marble cheese"
+        }
+      ]
+    }]
+  },
+  {
+    "id": 3,
+    "name": "Fries",
+    "image": "../assets/fries.png",
+    "price": 25,
+    "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    "attr": [{
+      "mandatory": true,
+      "multiple": false,
+      "title": {
+        "key": "size",
+        "value": "Size"
+      },
+      "item": [{
+          "key": "small",
+          "value": "Small"
+        },
+        {
+          "key": "medium",
+          "value": "Medium"
+        },
+        {
+          "key": "large",
+          "value": "Large"
+        }
+      ]
+    }]
+  },
+  {
+    "id": 4,
+    "name": "Coffee",
+    "image": "../assets/coffee.png",
+    "price": 40,
+    "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    "attr": [{
+        "mandatory": true,
+        "multiple": false,
+        "title": {
+          "key": "temp",
+          "value": "Temperature"
+        },
+        "item": [{
+            "key": "hot",
+            "value": "Hot"
+          },
+          {
+            "key": "cold",
+            "value": "Cold"
+          }
+        ]
+      },
+      {
+        "mandatory": true,
+        "multiple": false,
+        "title": "size",
+        "item": [{
+            "key": "small",
+            "value": "Small"
+          },
+          {
+            "key": "medium",
+            "value": "Medium"
+          },
+          {
+            "key": "large",
+            "value": "Large"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "id": 5,
+    "name": "Icecream Sundae",
+    "image": "../assets/milkshake.png",
+    "price": 65,
+    "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    "attr": [{
+        "mandatory": true,
+        "multiple": false,
+        "title": {
+          "key": "flavour",
+          "value": "Flavour"
+        },
+        "item": [{
+            "key": "chocolate",
+            "value": "Chocolate"
+          },
+          {
+            "key": "vanilla",
+            "value": "Vanilla"
+          },
+          {
+            "key": "strawberry",
+            "value": "Strawberry"
+          }
+        ]
+      },
+      {
+        "mandatory": false,
+        "multiple": true,
+        "title": "toppings",
+        "item": [{
+            "key": "mochi",
+            "value": "Mochi"
+          },
+          {
+            "key": "oreos",
+            "value": "Oreos"
+          },
+          {
+            "key": "sprinkles",
+            "value": "Sprinkles"
+          },
+          {
+            "key": "hot-fudge",
+            "value": "Hot fudge"
+          },
+          {
+            "key": "caramel",
+            "value": "Caramel"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "id": 6,
+    "name": "Fruit Juice",
+    "image": "../assets/juice.png",
+    "price": 30,
+    "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    "attr": [{
+      "mandatory": true,
+      "multiple": true,
+      "title": {
+        "key": "fruit",
+        "value": "Fruit"
+      },
+      "item": [{
+          "key": "apple",
+          "value": "Apple"
+        },
+        {
+          "key": "orange",
+          "value": "Orange"
+        },
+        {
+          "key": "carrot",
+          "value": "Carrot"
+        },
+        {
+          "key": "mango",
+          "value": "Mango"
+        },
+        {
+          "key": "Kiwi",
+          "value": "Kiwi"
+        },
+        {
+          "key": "banana",
+          "value": "Banana"
+        }
+      ]
+    }]
+  }
+]
