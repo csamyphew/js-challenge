853317b5c11e390509e991158766d079f457f471
csamyphew <csamyphew.cky@gmail.com>
2019-11-30 22:29:06 +0800

    Install packages

--- src/main.js
+++ src/main.js
@@ -2,7 +2,22 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 
+import BootstrapVue from 'bootstrap-vue'
+import VueCurrencyFilter from 'vue-currency-filter'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 Vue.config.productionTip = false
+Vue.use(BootstrapVue)
+Vue.use(VueCurrencyFilter, {
+  symbol: 'HK$',
+  thousandsSeparator: ',',
+  fractionCount: 0,
+  fractionSeparator: '.',
+  symbolPosition: 'front',
+  symbolSpacing: true
+})
 
 new Vue({
   store,
