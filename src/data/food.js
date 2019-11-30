484c5f0a7cd1f6e70306f81ec109ac7ffd3d796a
csamyphew <csamyphew.cky@gmail.com>
2019-11-30 23:41:03 +0800

    Implement basket state, getters, action, mutations

--- src/data/food.js
+++ src/data/food.js
@@ -1,240 +1,249 @@
 export const food_items = [{
-    "id": 1,
-    "name": "Milkshake",
-    "image": "../assets/milkshake.png",
-    "price": 30,
-    "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    "attr": [{
-        "mandatory": true,
-        "multiple": false,
-        "title": {
-          "key": "flavour",
-          "value": "Flavour"
+    id: 1,
+    name: "Milkshake",
+    image: "../assets/milkshake.png",
+    price: 30,
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    attr: [{
+        mandatory: true,
+        multiple: false,
+        title: {
+          key: "flavour",
+          value: "Flavour"
         },
-        "item": [{
-            "key": "chocolate",
-            "value": "Chocolate"
+        item: [{
+            key: "chocolate",
+            value: "Chocolate"
           },
           {
-            "key": "vanilla",
-            "value": "Vanilla"
+            key: "vanilla",
+            value: "Vanilla"
           },
           {
-            "key": "strawberry",
-            "value": "Strawberry"
+            key: "strawberry",
+            value: "Strawberry"
           }
         ]
       },
       {
-        "mandatory": true,
-        "multiple": false,
-        "title": "size",
-        "item": [{
-            "key": "small",
-            "value": "Small"
+        mandatory: true,
+        multiple: false,
+        title: {
+          key: "size",
+          value: "Size"
+        },
+        item: [{
+            key: "small",
+            value: "Small"
           },
           {
-            "key": "medium",
-            "value": "Medium"
+            key: "medium",
+            value: "Medium"
           },
           {
-            "key": "large",
-            "value": "Large"
+            key: "large",
+            value: "Large"
           }
         ]
       }
     ]
   },
   {
-    "id": 2,
-    "name": "Cale",
-    "image": "../assets/cake.png",
-    "price": 15,
-    "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    "attr": [{
-      "mandatory": true,
-      "multiple": false,
-      "title": {
-        "key": "flavour",
-        "value": "Flavour"
+    id: 2,
+    name: "Cale",
+    image: "../assets/cake.png",
+    price: 15,
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    attr: [{
+      mandatory: true,
+      multiple: false,
+      title: {
+        key: "flavour",
+        value: "Flavour"
       },
-      "item": [{
-          "key": "black-forest",
-          "value": "Black Forest"
+      item: [{
+          key: "black-forest",
+          value: "Black Forest"
         },
         {
-          "key": "mixed-fruit",
-          "value": "Mixed fruit"
+          key: "mixed-fruit",
+          value: "Mixed fruit"
         },
         {
-          "key": "marble-cheese",
-          "value": "Marble cheese"
+          key: "marble-cheese",
+          value: "Marble cheese"
         }
       ]
     }]
   },
   {
-    "id": 3,
-    "name": "Fries",
-    "image": "../assets/fries.png",
-    "price": 25,
-    "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    "attr": [{
-      "mandatory": true,
-      "multiple": false,
-      "title": {
-        "key": "size",
-        "value": "Size"
+    id: 3,
+    name: "Fries",
+    image: "../assets/fries.png",
+    price: 25,
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    attr: [{
+      mandatory: true,
+      multiple: false,
+      title: {
+        key: "size",
+        value: "Size"
       },
-      "item": [{
-          "key": "small",
-          "value": "Small"
+      item: [{
+          key: "small",
+          value: "Small"
         },
         {
-          "key": "medium",
-          "value": "Medium"
+          key: "medium",
+          value: "Medium"
         },
         {
-          "key": "large",
-          "value": "Large"
+          key: "large",
+          value: "Large"
         }
       ]
     }]
   },
   {
-    "id": 4,
-    "name": "Coffee",
-    "image": "../assets/coffee.png",
-    "price": 40,
-    "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    "attr": [{
-        "mandatory": true,
-        "multiple": false,
-        "title": {
-          "key": "temp",
-          "value": "Temperature"
+    id: 4,
+    name: "Coffee",
+    image: "../assets/coffee.png",
+    price: 40,
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    attr: [{
+        mandatory: true,
+        multiple: false,
+        title: {
+          key: "temp",
+          value: "Temperature"
         },
-        "item": [{
-            "key": "hot",
-            "value": "Hot"
+        item: [{
+            key: "hot",
+            value: "Hot"
           },
           {
-            "key": "cold",
-            "value": "Cold"
+            key: "cold",
+            value: "Cold"
           }
         ]
       },
       {
-        "mandatory": true,
-        "multiple": false,
-        "title": "size",
-        "item": [{
-            "key": "small",
-            "value": "Small"
+        mandatory: true,
+        multiple: false,
+        title: {
+          key: "size",
+          value: "Size"
+        },
+        item: [{
+            key: "small",
+            value: "Small"
           },
           {
-            "key": "medium",
-            "value": "Medium"
+            key: "medium",
+            value: "Medium"
           },
           {
-            "key": "large",
-            "value": "Large"
+            key: "large",
+            value: "Large"
           }
         ]
       }
     ]
   },
   {
-    "id": 5,
-    "name": "Icecream Sundae",
-    "image": "../assets/milkshake.png",
-    "price": 65,
-    "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    "attr": [{
-        "mandatory": true,
-        "multiple": false,
-        "title": {
-          "key": "flavour",
-          "value": "Flavour"
+    id: 5,
+    name: "Icecream Sundae",
+    image: "../assets/milkshake.png",
+    price: 65,
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    attr: [{
+        mandatory: true,
+        multiple: false,
+        title: {
+          key: "flavour",
+          value: "Flavour"
         },
-        "item": [{
-            "key": "chocolate",
-            "value": "Chocolate"
+        item: [{
+            key: "chocolate",
+            value: "Chocolate"
           },
           {
-            "key": "vanilla",
-            "value": "Vanilla"
+            key: "vanilla",
+            value: "Vanilla"
           },
           {
-            "key": "strawberry",
-            "value": "Strawberry"
+            key: "strawberry",
+            value: "Strawberry"
           }
         ]
       },
       {
-        "mandatory": false,
-        "multiple": true,
-        "title": "toppings",
-        "item": [{
-            "key": "mochi",
-            "value": "Mochi"
+        mandatory: false,
+        multiple: true,
+        title: {
+          key: "toppings",
+          value: "Toppings"
+        },
+        item: [{
+            key: "mochi",
+            value: "Mochi"
           },
           {
-            "key": "oreos",
-            "value": "Oreos"
+            key: "oreos",
+            value: "Oreos"
           },
           {
-            "key": "sprinkles",
-            "value": "Sprinkles"
+            key: "sprinkles",
+            value: "Sprinkles"
           },
           {
-            "key": "hot-fudge",
-            "value": "Hot fudge"
+            key: "hot-fudge",
+            value: "Hot fudge"
           },
           {
-            "key": "caramel",
-            "value": "Caramel"
+            key: "caramel",
+            value: "Caramel"
           }
         ]
       }
     ]
   },
   {
-    "id": 6,
-    "name": "Fruit Juice",
-    "image": "../assets/juice.png",
-    "price": 30,
-    "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    "attr": [{
-      "mandatory": true,
-      "multiple": true,
-      "title": {
-        "key": "fruit",
-        "value": "Fruit"
+    id: 6,
+    name: "Fruit Juice",
+    image: "../assets/juice.png",
+    price: 30,
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    attr: [{
+      mandatory: true,
+      multiple: true,
+      title: {
+        key: "fruit",
+        value: "Fruit"
       },
-      "item": [{
-          "key": "apple",
-          "value": "Apple"
+      item: [{
+          key: "apple",
+          value: "Apple"
         },
         {
-          "key": "orange",
-          "value": "Orange"
+          key: "orange",
+          value: "Orange"
         },
         {
-          "key": "carrot",
-          "value": "Carrot"
+          key: "carrot",
+          value: "Carrot"
         },
         {
-          "key": "mango",
-          "value": "Mango"
+          key: "mango",
+          value: "Mango"
         },
         {
-          "key": "Kiwi",
-          "value": "Kiwi"
+          key: "Kiwi",
+          value: "Kiwi"
         },
         {
-          "key": "banana",
-          "value": "Banana"
+          key: "banana",
+          value: "Banana"
         }
       ]
     }]
--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,25 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    baskets: [],
+    total: 0
+  },
+  getters: {
+    allBaskets: state => state.baskets,
+    total: state => state.total
   },
   mutations: {
+    addBasket(state, food) {
+      state.baskets.push(food);
+    },
+    updateTotal(state, price) {
+      state.total += price;
+    }
   },
   actions: {
-  },
-  modules: {
+    addFood({ commit }, food) {
+      commit('addBasket', food);
+      commit('updateTotal', food.quantity * food.price);
+    }
   }
 })
